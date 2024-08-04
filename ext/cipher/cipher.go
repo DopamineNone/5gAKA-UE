27fbe6ee9fefe9caeb4f99c58a3c6254803ec7f1
DopamineNone <c20160419raytheon@163.com>
2024-08-04 22:17:37 +0800

    feat extends cipher for 5GAKA

---
+++ ext/cipher/cipher.go
@@ -0,0 +1,56 @@
+package cipher
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+)
+
+// GenerateSUCI Generate Subscription Concealed Identifier(SUCI). (use null scheme)
+func GenerateSUCI(SUPI string) string {
+	// 对SUPI使用空保护策略
+	// SUCI=SUPI类型取值(0表示imsi)||归属网络标识符(mcc+mnc)||路由标识符||SUPI保护算法ID(0表示null scheme)||归属网络公钥||msin
+	mcc := SUPI[:3]
+	mnc := SUPI[3:5]
+	msin := SUPI[5:]
+	SUCI := "0" + mcc + mnc + "678" + "0" + "0" + msin
+	return SUCI
+}
+
+func ResolveAUTN(AUTN string) (string, string, string) {
+	sqnAK := AUTN[:12]
+	amf := AUTN[12:16]
+	mac := AUTN[16:]
+	return sqnAK, amf, mac
+}
+
+func CheckMac(xMacA, MacA string) int {
+	if xMacA == MacA {
+		return 1
+	} else {
+		return 0
+	}
+}
+
+func GenerateResStar(ck, ik, P0, L0, rand, res string) string {
+	key := []byte(ck + ik)
+	P1 := rand
+	L1 := fmt.Sprintf("%x", len(P1))
+	P2 := res
+	L2 := fmt.Sprintf("%x", len(P2))
+	s := []byte("6B" + P0 + L0 + P1 + L1 + P2 + L2)
+	h := hmac.New(sha256.New, key)
+	h.Write(s)
+	resStar := hex.EncodeToString(h.Sum(nil))[32:]
+	return resStar
+}
+
+func InitForUE() (string, string, string, string) {
+	ki := "000000012449900000000010123456d8"
+	op := "cda0c2852846d8eb63a387051cdd1fa5"
+	//global sn_name
+	snName := "123456789"
+	sqnMax := "100000000000000000000000"
+	return ki, op, snName, sqnMax
+}
